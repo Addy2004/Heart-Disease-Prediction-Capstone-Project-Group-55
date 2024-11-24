631beb1e1e9249ffc99fca16ba4b249c016dc744
Addy2004 <40354792+Addy2004@users.noreply.github.com>
2024-11-24 22:08:04 +0530

    initial commit

---
+++ Models/Support Vector Machines/Sigmoid Kernel/sigmoidKernelSVM.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.preprocessing import MinMaxScaler
+import pickle
+pd.set_option("display.max_rows", None)
+
+df = pd.read_csv("heart.csv")
+string_col = df.select_dtypes(include="object").columns
+df[string_col] = df[string_col].astype("string")
+
+string_col = df.select_dtypes("string").columns.to_list()
+num_col = df.columns.to_list()
+for col in string_col:
+    num_col.remove(col)
+num_col.remove("HeartDisease")
+
+df_nontree = pd.get_dummies(df,
+                            columns=string_col,
+                            drop_first=False)
+
+bool_cols = df_nontree.select_dtypes(include=['bool']).columns
+df_nontree[bool_cols] = df_nontree[bool_cols].astype(int)
+target = "HeartDisease"
+y = df_nontree[target].values
+df_nontree.drop("HeartDisease", axis=1, inplace=True)
+df_nontree = pd.concat([df_nontree, df[target]], axis=1)
+df_nontree.head()
+
+feature_col_nontree = df_nontree.columns.to_list()
+feature_col_nontree.remove(target)
+
+acc_svm = []
+kf = model_selection.StratifiedKFold(n_splits=5)
+for fold, (trn_, val_) in enumerate(kf.split(X=df_nontree, y=y)):
+    X_train = df_nontree.loc[trn_, feature_col_nontree]
+    y_train = df_nontree.loc[trn_, target]
+
+    X_valid = df_nontree.loc[val_, feature_col_nontree]
+    y_valid = df_nontree.loc[val_, target]
+
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_valid = scaler.transform(X_valid)
+
+    clf = SVC(kernel="sigmoid", random_state=69, probability=True)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_valid)
+
+    print(f"The fold is : {fold} : ")
+    print(classification_report(y_valid, y_pred))
+    acc = roc_auc_score(y_valid, y_pred)
+    acc_svm.append(acc)
+    print(f"The accuracy for {fold + 1} : {acc}")
+    pass
+
+pickle.dump(clf, open("sigmoidKernelSVM.pkl", "wb"))
---
+++ Models/XGBoost/xgboostModel.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn import model_selection
+from xgboost import XGBClassifier
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.preprocessing import LabelEncoder
+import pickle
+pd.set_option("display.max_rows", None)
+
+df = pd.read_csv("heart.csv")
+
+string_col = df.select_dtypes(include="object").columns
+df[string_col] = df[string_col].astype("string")
+string_col = df.select_dtypes("string").columns.to_list()
+num_col = df.columns.to_list()
+for col in string_col:
+    num_col.remove(col)
+num_col.remove("HeartDisease")
+
+df_tree = df.apply(LabelEncoder().fit_transform)
+target = "HeartDisease"
+y = df_tree[target].values
+df_tree.drop("HeartDisease", axis=1, inplace=True)
+df_tree = pd.concat([df_tree, df[target]], axis=1)
+df_tree.head()
+
+feature_col_tree = df_tree.columns.to_list()
+feature_col_tree.remove(target)
+
+acc_XGB = []
+kf = model_selection.StratifiedKFold(n_splits=5)
+for fold, (trn_, val_) in enumerate(kf.split(X=df_tree, y=y)):
+    X_train = df_tree.loc[trn_, feature_col_tree]
+    y_train = df_tree.loc[trn_, target]
+
+    X_valid = df_tree.loc[val_, feature_col_tree]
+    y_valid = df_tree.loc[val_, target]
+
+    clf = XGBClassifier()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_valid)
+    
+    print(f"The fold is : {fold} : ")
+    print(classification_report(y_valid, y_pred))
+    acc = roc_auc_score(y_valid, y_pred)
+    acc_XGB.append(acc)
+    print(f"The accuracy for {fold + 1} : {acc}")
+    pass
+
+pickle.dump(clf, open("xgboostModel.pkl", "wb"))
