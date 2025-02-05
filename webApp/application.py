5dc2e12bbb106504aba2985d030d34682bdc5bf9
Addy2004 <40354792+Addy2004@users.noreply.github.com>
2025-02-05 14:29:47 +0530

    huh

--- webApp/application.py
+++ webApp/application.py
@@ -1,6 +1,7 @@
+import os
 from application import create_app
 
-app = create_app()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app = create_app()
+    app.run(host='0.0.0.0', port=port, debug=True)
